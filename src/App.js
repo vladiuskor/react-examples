e468d46635e4ad4e9b970b0ddf6c76d5da1d458a
vladko <max.vlad.kor@gmail.com>
2022-01-24 12:36:42 +0200

    Added examples how use functions which works with state

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class WhoAmI extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            years: 27
+            years: 27,
+            position: ''
         }
     }
 
@@ -27,13 +28,23 @@ class WhoAmI extends Component {
         })
     }
 
+    commitInputChanges = (e, color) => {
+        console.log(color)
+        this.setState( {position: e.target.value});
+    }
+
     render() {
         const {name, surname, link} = this.props;
+        const {position, years} = this.state
         return (
             <div>
                 <button onClick={this.incrementYear}>+++</button>
-                <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+                <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
                 <a href={link}>My profile</a>
+                <form>
+                <span>Введите должность</span>
+                <input type="text" onChange={(e) => this.commitInputChanges(e, 'red')} />
+                </form>
             </div>
         )
     }
