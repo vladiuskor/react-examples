c19b9bafd55bc075671b41615fcd546b0731a4ea
vladko <max.vlad.kor@gmail.com>
2022-02-04 17:47:25 +0200

    Had practice with styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import {Component} from "react";
+import styled from "styled-components"
+
 import './App.css';
 
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, 0.2);
+  a {
+    display: block;
+    margin: 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2 `
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, 0.2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, 0.2);
+`
 
 // Functional component (in the React beginning - they couldn't have a state)
 
@@ -37,26 +67,31 @@ class WhoAmI extends Component {
         const {name, surname, link} = this.props;
         const {position, years} = this.state
         return (
-            <div>
-                <button onClick={this.incrementYear}>+++</button>
-                <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
+            <EmpItem active>
+                <Button onClick={this.incrementYear}>+++</Button>
+                <Header>My name is {name}, surname - {surname}, age - {years}, position - {position}</Header>
                 <a href={link}>My profile</a>
                 <form>
                 <span>Введите должность</span>
                 <input type="text" onChange={(e) => this.commitInputChanges(e, 'red')} />
                 </form>
-            </div>
+            </EmpItem>
         )
     }
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`
+
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
         <WhoAmI name="Vlad" surname="Korobko" link="facebook.com"/>
         <WhoAmI name="Roma" surname="Korobko" link="instagram.com"/>
-    </div>
+    </Wrapper>
   );
 }
 
