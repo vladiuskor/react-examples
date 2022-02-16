3811547ed95b80a25bcfce23c59b7586ee71f80a
vladko <max.vlad.kor@gmail.com>
2022-02-16 13:47:30 +0200

    Added ref examples

--- src/App.js
+++ src/App.js
@@ -1,174 +1,48 @@
-import React, {Component} from "react";
-import styled from "styled-components";
-import BootstrapTest from "./BootstrapTest";
-
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
+class Form extends Component {
+    // myRef = React.createRef();
+    // mySecondRef = React.createRef();
 
-const EmpItem = styled.div`
-  padding: 20px;
-  margin-bottom: 15px;
-  border-radius: 5px;
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+    // componentDidMount() {
+    //     this.myRef.current.focus();
+    // }
 
-  a {
-    display: block;
-    margin: 10px 0;
-    color: ${props => props.active ? 'orange' : 'black'};
-  }
+    setInputRef = (elem) => {
+        this.myRef = elem;
+    }
 
-  input {
-    display: block;
-    margin-top: 10px;
-  }
-`;
-
-const Header = styled.h2`
-  font-size: 22px;
-`;
-
-export const Button = styled.button`
-  display: block;
-  padding: 5px 15px;
-  background-color: gold;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
-`
-
-// Functional component (in the React beginning - they couldn't have a state)
-
-// function WhoAmI1({name, surname, link}) {
-//     return(
-//         <div>
-//             <h1>My name is {name}, surname - {surname}</h1>
-//             <a href={link}>My profile</a>
-//         </div>
-//     )
-// }
-
-class WhoAmI extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            years: 27,
-            position: ''
+    focusFirstTextInput = () => {
+        if(this.myRef) {
+            this.myRef.focus();
         }
     }
 
-    incrementYear = () => {
-        this.setState(state => {
-            return {years: state.years + 1}
-        })
-    }
-
-    commitInputChanges = (e, color) => {
-        console.log(color)
-        this.setState({position: e.target.value});
-    }
-
     render() {
-        const {name, surname, link} = this.props;
-        const {position, years} = this.state
         return (
-            <EmpItem active>
-                <Button onClick={this.incrementYear}>+++</Button>
-                <Header>My name is {name}, surname - {surname}, age - {years}, position - {position}</Header>
-                <a href={link}>My profile</a>
-                <form>
-                    <span>Введите должность</span>
-                    <input type="text" onChange={(e) => this.commitInputChanges(e, 'red')}/>
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input ref={this.setInputRef} type="email" className="form-control" id="exampleFormControlInput1"
+                               placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea onClick={this.focusFirstTextInput} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
                 </form>
-            </EmpItem>
+            </Container>
         )
     }
 }
-
-const Wrapper = styled.div`
-  width: 600px;
-  margin: 80px auto 0 auto;
-`
-
-const DynamicGreating = (props) => {
-    return (
-        <div className={'mb-3 p-3 border border-' + props.color}>
-            {/*{props.children}*/}
-
-            {
-                React.Children.map(props.children, child => {
-                    return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounded'})
-                })
-            }
-        </div>
-    )
-}
-
-const HelloGreating = () => {
-    return (
-        <div style={{'width': '600px', 'margin': '0 auto'}}>
-            <DynamicGreating color={'primary'}>
-                <h2>Hello world!</h2>
-            </DynamicGreating>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <h2>The counter is {props.counter}</h2>
-    )
-}
-
-class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    changeCounter = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
-
-    render() {
-        return(
-            <>
-                <button
-                    className={'btn btn-primary mb-2'}
-                    onClick={this.changeCounter}>
-                    Click me!
-                </button>
-                {this.props.render(this.state.counter)}
-            </>
-        )
-    }
-}
-
 
 function App() {
     return (
-        <Wrapper>
-            <Counter render={ counter => (
-                <Message counter={counter}/>
-            )}/>
-
-            <HelloGreating />
-            <BootstrapTest
-                left={
-                    <DynamicGreating color={'primary'}>
-                        <h2>This wheel was hard</h2>
-                        <h2>Hello world!</h2>
-                    </DynamicGreating>
-                }
-                right={
-                    <DynamicGreating color={'primary'}>
-                        <h2>Right!</h2>
-                    </DynamicGreating>
-                }
-            />
-
-            <WhoAmI name="Vlad" surname="Korobko" link="facebook.com"/>
-            <WhoAmI name="Roma" surname="Korobko" link="instagram.com"/>
-        </Wrapper>
+        <Form/>
     );
 }
 
