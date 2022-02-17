6d33ffa19ba86f4377cbf0791d4a6653a2cf7c19
vladko <max.vlad.kor@gmail.com>
2022-02-17 16:51:37 +0200

    Had practice with useState

--- src/App.js
+++ src/App.js
@@ -1,175 +1,64 @@
-import React, {Component} from "react";
-import styled from "styled-components";
-import BootstrapTest from "./BootstrapTest";
-
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
 
-const EmpItem = styled.div`
-  padding: 20px;
-  margin-bottom: 15px;
-  border-radius: 5px;
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+const Slider = (props) => {
 
-  a {
-    display: block;
-    margin: 10px 0;
-    color: ${props => props.active ? 'orange' : 'black'};
-  }
+    const [slide, setSlider ] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
 
-  input {
-    display: block;
-    margin-top: 10px;
-  }
-`;
 
-const Header = styled.h2`
-  font-size: 22px;
-`;
+    //Example with complex state. Less readable/ Not recommended
+    // const [state, setState] = useState({slide: 0, autoplay: false});
+    // function changeSlide(i) {
+    //     setState(state => ({...state, slide: state.slide + i}))
+    // }
+    // function toggleAutoplay() {
+    //     setState(state => ({...state, autoplay: !state.autoplay }));
+    // }
 
-export const Button = styled.button`
-  display: block;
-  padding: 5px 15px;
-  background-color: gold;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
-`
 
-// Functional component (in the React beginning - they couldn't have a state)
-
-// function WhoAmI1({name, surname, link}) {
-//     return(
-//         <div>
-//             <h1>My name is {name}, surname - {surname}</h1>
-//             <a href={link}>My profile</a>
-//         </div>
-//     )
-// }
-
-class WhoAmI extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            years: 27,
-            position: ''
-        }
+    // Without callback
+    function changeSlide(i) {
+        setSlider(slide + i)
     }
 
-    incrementYear = () => {
-        this.setState(state => {
-            return {years: state.years + 1}
-        })
+    // With callback
+    // function changeSlide(i) {
+    //     setSlider(slide => slide + i)
+    // }
+
+    function toggleAutoplay() {
+        setAutoplay(!autoplay);
     }
 
-    commitInputChanges = (e, color) => {
-        console.log(color)
-        this.setState({position: e.target.value});
-    }
-
-    render() {
-        const {name, surname, link} = this.props;
-        const {position, years} = this.state
-        return (
-            <EmpItem active>
-                <Button onClick={this.incrementYear}>+++</Button>
-                <Header>My name is {name}, surname - {surname}, age - {years}, position - {position}</Header>
-                <a href={link}>My profile</a>
-                <form>
-                    <span>Введите должность</span>
-                    <input type="text" onChange={(e) => this.commitInputChanges(e, 'red')}/>
-                </form>
-            </EmpItem>
-        )
-    }
-}
-
-const Wrapper = styled.div`
-  width: 600px;
-  margin: 80px auto 0 auto;
-`
-
-const DynamicGreating = (props) => {
     return (
-        <div className={'mb-3 p-3 border border-' + props.color}>
-            {/*{props.children}*/}
-
-            {
-                React.Children.map(props.children, child => {
-                    return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounded'})
-                })
-            }
-        </div>
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
+                <div className="buttons mt-3">
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={toggleAutoplay}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
     )
-}
-
-const HelloGreating = () => {
-    return (
-        <div style={{'width': '600px', 'margin': '0 auto'}}>
-            <DynamicGreating color={'primary'}>
-                <h2>Hello world!</h2>
-            </DynamicGreating>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <h2>The counter is {props.counter}</h2>
-    )
-}
-
-class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    changeCounter = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
-
-    render() {
-        return(
-            <>
-                <button
-                    className={'btn btn-primary mb-2'}
-                    onClick={this.changeCounter}>
-                    Click me!
-                </button>
-                {this.props.render(this.state.counter)}
-            </>
-        )
-    }
 }
 
 
 function App() {
-    return (
-        <Wrapper>
-            <Counter render={ counter => (
-                <Message counter={counter}/>
-            )}/>
-
-            <HelloGreating />
-            <BootstrapTest
-                left={
-                    <DynamicGreating color={'primary'}>
-                        <h2>This wheel was hard</h2>
-                        <h2>Hello world!</h2>
-                    </DynamicGreating>
-                }
-                right={
-                    <DynamicGreating color={'primary'}>
-                        <h2>Right!</h2>
-                    </DynamicGreating>
-                }
-            />
-
-            <WhoAmI name="Vlad" surname="Korobko" link="facebook.com"/>
-            <WhoAmI name="Roma" surname="Korobko" link="instagram.com"/>
-        </Wrapper>
-    );
+  return (
+        <Slider/>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Button} from './App';
-import styled from 'styled-components';
-// import reportWebVitals from './reportWebVitals';
 
-const BigButton = styled(Button)`
-  margin: 0 auto;
-  width: 245px;
-  text-align: center;
-`
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-        <BigButton as="a">Отправить отчёт</BigButton>
-    </React.StrictMode>,
-    document.getElementById('root')
+    <App/>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
